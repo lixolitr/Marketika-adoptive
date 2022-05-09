d410aa7b222d2cda07651e99eca53ad95a5f7258
Dmytro Lykholit <lixolitr@gmail.com>
2022-05-09 15:07:27 +0300

    starting kit for landing page with  adoptive design

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/*command to install in terminal: npm install --save-dev gulp gulp-sass sass browser-sync 
+command to start watch: gulp watch
+*/
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function style() {
+  return gulp
+    .src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
